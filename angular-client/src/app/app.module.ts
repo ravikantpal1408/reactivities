ecf23b29224c349c3fbbcfb0d235ca2561d05a6a
ravikantpal1408 <ravikantpal123@gmail.com>
2020-03-30 19:50:17 +0530

    added angular client

---
+++ angular-client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { RegisterComponent } from './modules/account/register/register.component';
+import { LoginComponent } from './modules/account/login/login.component';
+import { HomeComponent } from './modules/home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
