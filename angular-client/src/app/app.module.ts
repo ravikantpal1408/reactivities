7e2a767dcb1c87b150503aa08cf9bc86695d8997
ravikantpal1408 <ravikantpal123@gmail.com>
2020-03-30 22:40:45 +0530

    added further more components

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { TopNavComponent } from './layout/top-nav/top-nav.component';
+import { SideNavComponent } from './layout/side-nav/side-nav.component';
 import { RegisterComponent } from './modules/account/register/register.component';
 import { LoginComponent } from './modules/account/login/login.component';
 import { HomeComponent } from './modules/home/home.component';
@@ -6,17 +8,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialForReactivities } from './angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    SideNavComponent,
+    TopNavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialForReactivities
   ],
   providers: [],
   bootstrap: [AppComponent]
